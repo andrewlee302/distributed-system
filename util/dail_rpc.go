eaa0127e496ad29b85d4e334740108fea8b36dc0
Andrew Lee <lichundian@gmail.com>
2018-03-22 16:06:34 +0800

    add resource pool and rpc call moudles.

---
+++ util/dail_rpc.go
@@ -0,0 +1,57 @@
+package util
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+	"syscall"
+)
+
+func DialServer(network, addr string) *rpc.Client {
+	var err error
+	rpcClient, err := rpc.Dial(network, addr)
+	if err != nil {
+		err1 := err.(*net.OpError)
+		if err1.Err != syscall.ENOENT && err1.Err != syscall.ECONNREFUSED {
+			fmt.Printf("Dial(%v) failed: %v\n", addr, err1)
+		}
+		return nil
+	}
+	return rpcClient
+}
+
+func RPCPoolCall(pool *ResourcePool, name string, args interface{}, reply interface{}) bool {
+	c := pool.Get().(*rpc.Client)
+	err := c.Call(name, args, reply)
+	if err == nil {
+		pool.Put(c)
+		return true
+	}
+	c.Close()
+	pool.Clean(c)
+	fmt.Println(err)
+	return false
+}
+
+func RPCPoolArrayCall(pa *ResourcePoolsArray, i int, name string, args interface{}, reply interface{}) bool {
+	c := pa.Get(i).(*rpc.Client)
+	err := c.Call(name, args, reply)
+	if err == nil {
+		pa.Put(i, c)
+		return true
+	}
+	c.Close()
+	pa.Clean(i, c)
+	fmt.Println(err)
+	return false
+}
+
+func RPCCall(c *rpc.Client, name string, args interface{}, reply interface{}) bool {
+	err := c.Call(name, args, reply)
+	if err == nil {
+		return true
+	}
+	c.Close()
+	fmt.Println(err)
+	return false
+}
---
+++ util/resouce_pool.go
@@ -0,0 +1,87 @@
+package util
+
+import (
+	"container/list"
+	"sync"
+	"sync/atomic"
+)
+
+type Resource interface {
+	Close() error
+}
+
+type ResourcePool struct {
+	New        func() Resource
+	entities   *list.List
+	maxSize    int64
+	activeSize int64
+	mu         sync.Mutex
+	cond       sync.Cond
+}
+
+func NewResourcePool(new func() Resource, maxSize int) *ResourcePool {
+	pool := ResourcePool{New: new, entities: list.New(),
+		maxSize: int64(maxSize), activeSize: 0}
+	pool.cond = sync.Cond{L: &pool.mu}
+	return &pool
+}
+
+func (pool *ResourcePool) Put(r Resource) {
+	pool.mu.Lock()
+	defer pool.mu.Unlock()
+	pool.entities.PushBack(r)
+	pool.cond.Broadcast()
+}
+
+func (pool *ResourcePool) Get() Resource {
+	pool.mu.Lock()
+
+	for pool.entities.Front() == nil && atomic.LoadInt64(&pool.activeSize) >= pool.maxSize {
+		pool.cond.Wait()
+	}
+	ele := pool.entities.Front()
+	if ele == nil {
+		atomic.AddInt64(&pool.activeSize, 1)
+		pool.mu.Unlock()
+		entity := pool.New()
+		if entity == nil {
+			atomic.AddInt64(&pool.activeSize, -1)
+			return nil
+		}
+		return entity
+	}
+	c := ele.Value.(Resource)
+	pool.entities.Remove(ele)
+	pool.mu.Unlock()
+	return c
+}
+
+func (pool *ResourcePool) Clean(r Resource) {
+	r.Close()
+	atomic.AddInt64(&pool.activeSize, -1)
+	pool.cond.Broadcast()
+}
+
+type ResourcePoolsArray struct {
+	pools []*ResourcePool
+}
+
+func NewResourcePoolsArray(news []func() Resource, maxSizeForOne, poolNum int) *ResourcePoolsArray {
+	pools := make([]*ResourcePool, poolNum)
+	for i := 0; i < poolNum; i++ {
+		pools[i] = NewResourcePool(news[i], maxSizeForOne)
+	}
+	return &ResourcePoolsArray{pools: pools}
+}
+
+func (array ResourcePoolsArray) Get(i int) Resource {
+	return array.pools[i].Get()
+}
+
+func (array ResourcePoolsArray) Put(i int, r Resource) {
+	array.pools[i].Put(r)
+}
+
+func (array ResourcePoolsArray) Clean(i int, r Resource) {
+	array.pools[i].Clean(r)
+}
